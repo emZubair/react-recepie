98e9c1b1cf81f1d14d489338f899d106c34a1b03
emzubair <muhammad.zubair@arbisoft.com>
2021-09-06 07:27:13 +0500

    Update API (patch), delete and env file added

--- src/pizzarias/pizzdetails.js
+++ src/pizzarias/pizzdetails.js
@@ -1,21 +1,47 @@
 import React, {Component} from "react";
-import PizzaList from "./pizzarias";
+import PizzaUpdate from './pizzeria_update';
 
 
 class PizzaDetails extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            showUpdate: false,
+            pizzobj: this.props.pizzobj
+        }
+    }
+
     render() {
-        const pizzaDetails = this.props.pizzDetails;
+        const pizzobj = this.props.pizzobj;
         return (
             <div style={{color: "yellow", border:"1px solid yellow"}}>
-                <h4>{pizzaDetails.pizzeria_name}</h4>
+                <button style={{backgroundColor: "white"}}
+                onClick={() => this.updatePizzeriaDetails()}
+                >Update Details</button>
+
+                <button style={{backgroundColor: "white"}}
+                onClick={() => this.handleDelete()}
+                >Delete Pizza</button>
+
+                <h4>{pizzobj.pizzeria_name}</h4>
                 <h5>
-                    Address: {pizzaDetails.city} - {pizzaDetails.state}
+                    Address: {pizzobj.city} - {pizzobj.state}
                 </h5>
 
-                <p>{pizzaDetails.description}</p>
+                <p>{pizzobj.description}</p>
 
+            {this.state.showUpdate ? (<PizzaUpdate obj={this.state.pizzobj} />): null}
             </div>
         )
+    }
+
+    updatePizzeriaDetails() {        
+        this.setState({showUpdate: !this.state.showUpdate})
+    }
+
+    handleDelete(){
+        this.props.onDelete(this.state.pizzobj)
     }
 }
 
---
+++ src/pizzarias/pizzeria_update.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import React, {Component} from "react";
+
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+class PizzaUpdate extends Component{
+    constructor(props){
+        super(props)
+        console.log("Receive object "+this.props.obj.pizza_name)
+
+        this.state = {
+            pizza_object: this.props.obj,
+            pizza_name: this.props.obj.pizza_name,
+            description: this.props.obj.description,
+            city: this.props.obj.city,
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+    }
+
+    render() {
+        console.log("Pizza update render:"+ this.state.pizza_name)
+        return(
+            <div className="pizza-update">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="update-details">
+                        Details
+                        <input type="text" onChange={this.handleUpdate} name="description" value={this.state.description}></input>
+                    </div>
+
+                    <div className="update-city">
+                        City
+                        <input type="text" name="city" onChange={this.handleUpdate} value={this.state.city}></input>
+                    </div>
+
+                    <input type="submit" value="Update Details"></input>
+                </form>
+            </div>
+        )
+    }
+
+    handleUpdate(event) {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    handleSubmit(event){
+        event.preventDefault()
+        axios
+        .patch(SERVER_URL.concat(this.state.pizza_object.update), 
+            {
+                details: this.state.details,
+                city: this.state.city
+            }
+        ).then((response) => {
+            console.log(response)
+        })
+        .catch(function (error){
+            console.log(error)
+        })
+    }
+}
+
+export default PizzaUpdate;
